28d2a45f2adafed54e309b8efaf24cb3f42058b5
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2024-05-03 20:36:50 +0300

    add show login logs in developer page

--- src/developer/pages/User.tsx
+++ src/developer/pages/User.tsx
@@ -1,11 +1,31 @@
-import { Typography } from "antd";
-import { useState } from "react";
+import { List, Typography } from "antd";
+import { useCallback, useMemo, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import logger, { Log } from "../../backend/logger";
 import { getUserById } from "../../backend/users";
+import s from "./User.module.css";
 
 const DeveloperUser = () => {
   const { userId } = useParams();
   const [user] = useState(() => getUserById(Number(userId)));
+
+  const logs: Log[] = useMemo(() => {
+    if (!user) {
+      return [];
+    }
+    return logger.getLogsByUser(user.id);
+  }, [user]);
+
+  const renderLogItem = useCallback((item: Log) => {
+    return (
+      <div className={s["log-item"]}>
+        <span className={s["log-date"]}>
+          {item.date.toLocaleDateString()} {item.date.toLocaleTimeString()}
+        </span>
+        <span>{item.action}</span>
+      </div>
+    );
+  }, []);
 
   return (
     <>
@@ -13,6 +33,13 @@ const DeveloperUser = () => {
         [{userId}] {user?.type} {user?.name}
       </Typography.Title>
       <Link to="/main">Go back</Link>
+      <List
+        size="small"
+        bordered
+        dataSource={logs}
+        renderItem={renderLogItem}
+        header={<Typography.Text>User logs</Typography.Text>}
+      />
     </>
   );
 };
