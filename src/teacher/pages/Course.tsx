7fe54550c0573e5817604c618d6ffcd8558a8bd3
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2024-05-05 17:06:34 +0300

    Add material and test on course page (#5)
    * Add course material and test in student app
    
    * Add list of students on course in the teacher app

--- src/teacher/pages/Course.tsx
+++ src/teacher/pages/Course.tsx
@@ -1,16 +1,37 @@
-import { Typography } from "antd";
-import { useState } from "react";
+import { Typography, Collapse, CollapseProps } from "antd";
+import { useMemo, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { getCourseById } from "../../backend/courses";
 
 const TeacherCourse = () => {
   const { courseId } = useParams();
   const [course] = useState(() => getCourseById(Number(courseId)));
+  const studentOnCourse = useMemo<CollapseProps["items"]>(
+    () =>
+      course?.students.map((student, index) => ({
+        key: index,
+        label: student.name,
+        children: <span>Место для протокола студента</span>,
+      })) ?? [],
+    [course]
+  );
 
   return (
     <>
       <Typography.Title level={3}>{course?.name}</Typography.Title>
       <Link to="/main">Go back</Link>
+      {course && (
+        <>
+          <div>
+            <Typography.Title level={4}>Материалы лекции:</Typography.Title>
+            <div>{course.material.lecture.text}</div>
+          </div>
+          <div>
+            <Typography.Title level={4}>Студенты на курсе</Typography.Title>
+            <Collapse items={studentOnCourse} />
+          </div>
+        </>
+      )}
     </>
   );
 };
